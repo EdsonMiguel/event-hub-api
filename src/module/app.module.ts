bf7728ece6ccf0d2239eb4c749f8d411673fd057
Edson Miguel <edson.silva@multicomp.com.br>
2026-06-06 09:39:26 -0300

    feat(database): cria módulo de conexão com PostgreSQL

--- src/module/app.module.ts
+++ src/module/app.module.ts
@@ -3,8 +3,26 @@ import { UserModule } from './users/user.module';
 import { RegistrationModule } from './registrations/registration.module';
 import { EventModule } from './events/event.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
-  imports: [AuthModule, EventModule, RegistrationModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.required(),
+        DB_PORT: Joi.number().default(5432),
+        DB_DATABASE: Joi.required(),
+        DB_USER: Joi.required(),
+        DB_PASS: Joi.required(),
+        DB_AUTO_LOAD: Joi.number().min(0).max(1).default(0),
+        DB_SYNC: Joi.number().min(0).max(1).default(0),
+      }),
+    }),
+    AuthModule,
+    EventModule,
+    RegistrationModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
